f24ca81dbb98907f9597fd4505d33284d6a33cab
LymoneLM <chengziorange233@qq.com>
2025-07-16 09:50:42 +0800

    feat(script/ui): 实现画面按键反向绑定输入

--- assets/script/ui/UIDirection.ts
+++ assets/script/ui/UIDirection.ts
@@ -1,5 +1,5 @@
 import { _decorator, Component, Node, director } from 'cc';
-import { Direction } from "../core/GameManager";
+import { Direction, GameManager } from "../core/GameManager";
 const { ccclass, property } = _decorator;
 
 @ccclass('UIDirection')
@@ -12,12 +12,32 @@ export class UIDirection extends Component {
     key_left: Node = null;
     @property({ type: Node, tooltip: '右键'})
     key_right: Node = null;
+    @property({ type: GameManager, tooltip: '游戏管理器' })
+    gm: GameManager = null;
 
     private set: Node[] = [];
 
     onLoad() {
         this.set = [this.key_up, this.key_down, this.key_left, this.key_right];
         director.on("direction", this.onDirectionKeyChanged, this);
+
+        // 绑定UI按钮点击事件
+        this.set.forEach((btn, dir) => {
+            if (btn) {
+                btn.on(Node.EventType.TOUCH_START, () => {
+                    director.emit("direction", dir, true);
+                    if (this.gm) {
+                        this.gm.updateSnakeDirection(dir);
+                    }
+                }, this);
+                btn.on(Node.EventType.TOUCH_END, () => {
+                    director.emit("direction", dir, false);
+                }, this);
+                btn.on(Node.EventType.TOUCH_CANCEL, () => {
+                    director.emit("direction", dir, false);
+                }, this);
+            }
+        });
     }
 
     onDestroy () {
